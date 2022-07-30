d2ced99c0cde16fb7b7db08ae270859ee2bd8b11
Mark-Nelovka <87484591+Mark-Nelovka@users.noreply.github.com>
2022-07-31 00:58:51 +0300

    add route for currency

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
 
-const authRouter = require("./routes/api/auth");
-const usersRouter = require("./routes/api/users");
-const transactionsRouter = require("./routes/api/transactions");
+const authRouter = require('./routes/api/auth');
+const usersRouter = require('./routes/api/users');
+const transactionsRouter = require('./routes/api/transactions');
+const currencyRouter = require('./routes/api/currency');
 
 const app = express();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
 app.use(cors());
@@ -17,16 +18,17 @@ app.use(express.json());
 
 // для реєстрації та авторизації будуть окремі роути - auth -це загальнопринята назва для маршруту авторизації
 // окремий ендпойнт
-app.use("/api/auth", authRouter);
-app.use("/api/users", usersRouter);
-app.use("/api/transactions", transactionsRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/transactions', transactionsRouter);
+app.use('/api/wallet_frontend', currencyRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+  res.status(404).json({ message: 'Not found' });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
+  const { status = 500, message = 'Server error' } = err;
   res.status(status).json({ message });
 });
 
