fbe921e2b1f844782ce7225c3fa23d2d8e146a66
Maxim <m.steblyna@omisoft.net>
2022-07-30 16:37:44 +0300

    fix transactions routes, model and controllers, and model and route for refresh token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -51,7 +51,7 @@ const auth = async (req, res, next) => {
     req.user = user;
     next();
   } catch (error) {
-    if (error.message === "Invalid sugnature") {
+    if (error.message === "Invalid signature") {
       error.status = 401;
     }
     next(error);
--- models/transaction.js
+++ models/transaction.js
@@ -3,48 +3,45 @@ const {Schema, model} = require("mongoose");
 const Joi = require("joi");
 
 const transactionSchema = Schema({
-    date: {
-        type: Date,
-        default: Date.now,
-        required: true,
-    },
-    transType: {
-        type: Boolean,
-        default: false,
-        required: true,
-    },
-    category: {
-        type: String,
-        required: true,
-    },
-    comment: {
-        type: String,
-    },
-    sum: {
-        type: Number,
-        required: true,
-    },
-    balance: {
-        type: Number,
-        required: true,
-    },
-    owner: {
-        type: Schema.Types.ObjectId,
-        ref: "user"
-    }
-})
+  date: {
+    type: Date,
+    default: Date.now(),
+    required: true,
+  },
+  transType: {
+    type: Boolean,
+    default: false,
+  },
+  category: {
+    type: String,
+  },
+  comment: {
+    type: String,
+  },
+  sum: {
+    type: Number,
+  },
+  balance: {
+    type: Number,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "user"
+  }
+}, {versionKey: false})
 
 const transactionJoiSchema = Joi.object({
-    transType: Joi.boolean().required(),
-    category: Joi.string().required(),
-    comment: Joi.string(),
-    sum: Joi.number().required(),
+  transType: Joi.boolean(),
+  category: Joi.string().required(),
+  comment: Joi.string(),
+  sum: Joi.number().required(),
+  balance: Joi.number().required()
 })
 
 
 const Transaction = model("transaction", transactionSchema);
 
 module.exports = {
-    Transaction,
-    transactionJoiSchema
+  Transaction,
+  transactionJoiSchema
 };
