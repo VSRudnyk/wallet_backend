e1e4706919621087095cf33a982da878d9b6a432
Vladimir Rudnyk <v.rudnyk@gmail.com>
2022-08-03 12:51:46 +0300

    Update categories.js
    added ctrlWrapper to router

--- routes/api/categories.js
+++ routes/api/categories.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
+const {ctrlWrapper} = require("../../helpers");
+
 const getCategories = require('../../controllers/categories/getCategories');
 const auth = require('../../middlewares/auth')
 
 const router = express.Router();
 
 
-router.get('/', auth, getCategories);
+router.get('/', auth, ctrlWrapper(ctrl.getCategories));
 
 module.exports = router;
