bbb943c6a48595b24df28f25076ccad671294d99
VSRudnyk <v.rudnyk@gmail.com>
2022-08-03 13:19:28 +0300

    added ctrlWrapper into categories router

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
-const auth = require("./auth");
-const users = require("./users");
-const transactions = require("./transactions");
+const auth = require('./auth');
+const users = require('./users');
+const transactions = require('./transactions');
+const categories = require('./categories');
 
 module.exports = {
   auth,
   transactions,
   users,
+  categories,
 };
--- routes/api/categories.js
+++ routes/api/categories.js
@@ -1,12 +1,11 @@
 const express = require('express');
 
-const {ctrlWrapper} = require("../../helpers");
+const auth = require('../../middlewares/auth');
+const { ctrlWrapper } = require('../../helpers');
 
-const getCategories = require('../../controllers/categories/getCategories');
-const auth = require('../../middlewares/auth')
+const { categories: ctrl } = require('../../controllers');
 
 const router = express.Router();
-
 
 router.get('/', auth, ctrlWrapper(ctrl.getCategories));
 
