e1bf4198a66c38c64c5ced13e81b3a8605ea3d72
Mark-Nelovka <87484591+Mark-Nelovka@users.noreply.github.com>
2022-08-04 16:52:11 +0300

    add current date

--- controllers/currency/currency.js
+++ controllers/currency/currency.js
@@ -1,8 +1,13 @@
 const { default: axios } = require('axios');
 
+const date = new Date();
+const day = date.getDay();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+
 const getCurrency = async (req, res) => {
   const data = await axios(
-    'https://api.privatbank.ua/p24api/exchange_rates?json&date=01.12.2014'
+    `https://api.privatbank.ua/p24api/exchange_rates?json&date=${day}.${month}.${year}`
   );
   res.status(200).json(data.data);
 };
